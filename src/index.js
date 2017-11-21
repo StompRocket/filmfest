0869cfad2967de462af7fe0ed7998129b346f032
Rosho123 <rsf.sho@gmail.com>
2017-11-20 21:23:01 -0800

    Beta

---
+++ src/index.js
@@ -0,0 +1,7 @@
+require('../node_modules/bulma/css/bulma.css')
+// require('../node_modules/aos/dist/aos.css')
+require('./styles.scss')
+// require('../node_modules/aos/dist/aos.js')
+// import AOS from './aos.js '
+require('./main.js')
+import slate from './slate.jpg'
---
+++ webpack.common.js
@@ -0,0 +1,64 @@
+const path = require('path')
+var webpack = require('webpack')
+module.exports = {
+  entry: {
+    app: './src/index.js'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    })
+  ],
+  module: {
+
+    loaders: [{
+      test: /\.scss$/,
+      use: [{
+        loader: 'style-loader' // creates style nodes from JS strings
+      }, {
+        loader: 'css-loader' // translates CSS into CommonJS
+      }, {
+        loader: 'sass-loader' // compiles Sass to CSS
+      }]
+    },
+    {
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader'
+      ]
+    },
+    {
+      test: /\.(png|svg|jpg|gif)$/,
+      use: [
+        'file-loader', {
+          loader: 'image-webpack-loader',
+          options: {
+            mozjpeg: {
+              progressive: true,
+              quality: 20
+            },
+        // optipng.enabled: false will disable optipng
+            optipng: {
+              enabled: false
+            },
+            pngquant: {
+              quality: '65-90',
+              speed: 4
+            },
+            gifsicle: {
+              interlaced: false
+            }
+
+          }
+        }
+      ]
+    }
+    ]
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist/')
+  }
+}
