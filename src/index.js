fc2ce4b40937382ac033a0f3d72a3c7433d752b8
Rosho123 <rsf.sho@gmail.com>
2017-12-11 20:03:05 -0800

    changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 require('../node_modules/bulma/css/bulma.css')
-require('../node_modules/video.js/dist/video-js.min.css')
-require('../node_modules/video.js/dist/video.min.js')
-// require('../node_modules/aos/dist/aos.css')
+
 require('./styles.scss')
-// require('../node_modules/aos/dist/aos.js')
-// import AOS from './aos.js '
+
 require('./main.js')
 import slate from './slate.jpg'
--- src/main.js
+++ src/main.js
@@ -1,13 +1,8 @@
 
 let resize = function () {
-  let player = document.getElementById('trailer')
-  let container = document.getElementById('video')
-  let controls = document.getElementsByClassName('vjs-controls-enabled')[0]
-  let playerTwo = document.getElementById('trailer_html5_api')
-  player.style.width = container.clientWidth + 'px'
-  controls.style.height = playerTwo.clientHeight + 'px'
-  console.log(controls.clientHeight + ' container height ' + playerTwo.clientHeight)
-  console.log(player.clientWidth + ' container ' + container.clientWidth)
+  let player = document.getElementById('videoPlayer')
+console.log(player.clientWidth);
+  player.style.height = (player.clientWidth / (16 / 9)) + 'px'
 }
 window.onload = resize
 window.onresize = resize
